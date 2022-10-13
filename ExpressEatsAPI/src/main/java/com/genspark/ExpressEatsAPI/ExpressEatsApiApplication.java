0593981b5be1a2251ca0f374d0b9cf4a1f95b48e
Elijah Piper <35882341+Elijah-Piper@users.noreply.github.com>
2022-10-13 09:07:26 -0700

    Created Spring Boot API skeleton

---
+++ ExpressEatsAPI/src/main/java/com/genspark/ExpressEatsAPI/ExpressEatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.ExpressEatsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressEatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressEatsApiApplication.class, args);
+	}
+
+}
---
+++ ExpressEatsAPI/src/test/java/com/genspark/ExpressEatsAPI/ExpressEatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.ExpressEatsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressEatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
